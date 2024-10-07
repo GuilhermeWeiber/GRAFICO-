0f5933d146529ae97a508288001187c535991680
GuilhermeWeiber <160506709+GuilhermeWeiber@users.noreply.github.com>
2024-10-07 11:40:57 -0300

    Add files via upload

--- graficos.js
+++ graficos.js
@@ -30,3 +30,33 @@ const graficoPizza = new Chart(ctxPizza, {
 
 
 const ctxBarra = document.getElementById('graficoBarra').getContext('2d');
+const graficoBarra = new Chart(ctxBarra, {
+    type: 'bar',
+    data: {
+        labels: ['Facebook', 'Instagram', 'Twitter', 'LinkedIn'],
+        datasets: [{
+            label: 'Usuários ativos (em milhões)',
+            data: [2800, 1500, 330, 810],
+            backgroundColor: '#F05454',
+            borderColor: '#30475E',
+            borderwidth: 1
+        }]
+    },
+    options: {
+        responsive: true,
+        plugins: {
+            legend: {
+                position: 'top',
+            },
+            title: {
+                display: true,
+                text: 'Usuários Ativos nas Redes Soriais (milhões)'
+            }
+        },
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
