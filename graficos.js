b8351d3c904d10fe73a3ff2008d411417a50bec1
GuilhermeWeiber <160506709+GuilhermeWeiber@users.noreply.github.com>
2024-10-22 09:09:10 -0300

    Add files via upload

--- graficos.js
+++ graficos.js
@@ -1,11 +1,11 @@
-const ctxPiza = document.getElementById('graficoPizza').getContext('2d');
+const ctxPizza = document.getElementById('graficoPizza').getContext('2d');
 const graficoPizza = new Chart(ctxPizza, {
     type: 'pie',
     data: {
-        labels: ['Facebook', 'Instagram', 'Twitter', 'LinkedIn'],
+        labels: ['Sim', 'Não', 'As vezes'],
         datasets: [{
-            label: 'Participação nas Redes Sociais',
-            data: [45, 30, 15, 10],
+            label: 'Você pratica atividades físicas',
+            data: [55, 11, 33],
             backgroundColor: [
                 '#F05454', '#30475E', '#DDDDDD', '#222831'
             ],
@@ -21,42 +21,153 @@ const graficoPizza = new Chart(ctxPizza, {
             },
             title: {
                 display: true,
-                text: 'Participação nas Redes Sociais'
+                text: 'Você pratica atividades físicas'
             }
         }
     }
 })
 
-
-
-const ctxBarra = document.getElementById('graficoBarra').getContext('2d');
-const graficoBarra = new Chart(ctxBarra, {
-    type: 'bar',
+const ctxPizza = document.getElementById('graficoPizza').getContext('2d');
+const graficoPizza = new Chart(ctxPizza, {
+    type: 'pie',
     data: {
-        labels: ['Facebook', 'Instagram', 'Twitter', 'LinkedIn'],
+        labels: ['Musculação', 'Volei', 'Caminhar', 'Futebol', 'Outros', 'Nada'],
         datasets: [{
-            label: 'Usuários ativos (em milhões)',
-            data: [2800, 1500, 330, 810],
-            backgroundColor: '#F05454',
-            borderColor: '#30475E',
-            borderwidth: 1
+            label: 'Quais as atividades que você pratica?',
+            data: [389, 389, 56, 56, 56, 56],
+            backgroundColor: [
+                '#F05454', '#30475E', '#DDDDDD', '#222831'
+            ],
+            borderColor: '#DDDDDD',
+            borderWidth: 1
         }]
     },
     options: {
         responsive: true,
         plugins: {
             legend: {
-                position: 'top',
+                position: 'bottom',
             },
             title: {
                 display: true,
-                text: 'Usuários Ativos nas Redes Soriais (milhões)'
-            }
-        },
-        scales: {
-            y: {
-                beginAtZero: true
+                text: 'Quais as atividades que você pratica?'
             }
         }
     }
-});
+})
+
+const ctxPizza = document.getElementById('graficoPizza').getContext('2d');
+const graficoPizza = new Chart(ctxPizza, {
+    type: 'pie',
+    data: {
+        labels: ['1 a 2 vezes', '3 a 5 vezes', '6 a 7 vezes'],
+        datasets: [{
+            label: 'Quantas vezes na semana?',
+            data: [44, 50, 5],
+            backgroundColor: [
+                '#F05454', '#30475E', '#DDDDDD', '#222831'
+            ],
+            borderColor: '#DDDDDD',
+            borderWidth: 1
+        }]
+    },
+    options: {
+        responsive: true,
+        plugins: {
+            legend: {
+                position: 'bottom',
+            },
+            title: {
+                display: true,
+                text: 'Quantas vezes na semana?'
+            }
+        }
+    }
+})
+
+const ctxPizza = document.getElementById('graficoPizza').getContext('2d');
+const graficoPizza = new Chart(ctxPizza, {
+    type: 'pie',
+    data: {
+        labels: ['menos de 1 ano', 'de 1 a 2 anos', 'mais de 3 anos', '10 anos', 'desde que aprendi a andar'],
+        datasets: [{
+            label: 'a quanto tempo voce pratica atividades fisicas?',
+            data: [38, 38, 11, 5, 5],
+            backgroundColor: [
+                '#F05454', '#30475E', '#DDDDDD', '#222831'
+            ],
+            borderColor: '#DDDDDD',
+            borderWidth: 1
+        }]
+    },
+    options: {
+        responsive: true,
+        plugins: {
+            legend: {
+                position: 'bottom',
+            },
+            title: {
+                display: true,
+                text: 'a quanto tempo voce pratica atividades fisicas?'
+            }
+        }
+    }
+})
+
+const ctxPizza = document.getElementById('graficoPizza').getContext('2d');
+const graficoPizza = new Chart(ctxPizza, {
+    type: 'pie',
+    data: {
+        labels: ['quero perder peso', 'quero ganhar peso', 'quero manter o fisico', 'pra zoar com os guri', 'quero manter minha saude', 'para ser melhor que os outros'],
+        datasets: [{
+            label: 'por que voce comecou a praticar?',
+            data: [22, 22, 27, 10, 5, 5],
+            backgroundColor: [
+                '#F05454', '#30475E', '#DDDDDD', '#222831'
+            ],
+            borderColor: '#DDDDDD',
+            borderWidth: 1
+        }]
+    },
+    options: {
+        responsive: true,
+        plugins: {
+            legend: {
+                position: 'bottom',
+            },
+            title: {
+                display: true,
+                text: 'por que voce comecou a praticar?'
+            }
+        }
+    }
+})
+
+const ctxPizza = document.getElementById('graficoPizza').getContext('2d');
+const graficoPizza = new Chart(ctxPizza, {
+    type: 'pie',
+    data: {
+        labels: ['sim', 'nao', 'volei pae', 'damas', 'xadrez'],
+        datasets: [{
+            label: 'voce esta tentando melhorar sua performance em algum esporte?',
+            data: [55, 27, 5, 5, 5],
+            backgroundColor: [
+                '#F05454', '#30475E', '#DDDDDD', '#222831'
+            ],
+            borderColor: '#DDDDDD',
+            borderWidth: 1
+        }]
+    },
+    options: {
+        responsive: true,
+        plugins: {
+            legend: {
+                position: 'bottom',
+            },
+            title: {
+                display: true,
+                text: 'voce esta tentando melhorar sua performance em algum esporte?'
+            }
+        }
+    }
+})
